beed6dab930094f368809df037c38003366e70f5
qlobafftar <qlobafftar@gmail.com>
2021-10-27 10:19:00 +0300

    Update parse.go

--- stmt/parse.go
+++ stmt/parse.go
@@ -91,9 +91,6 @@ func readString(r []rune, i, end int, quote rune, tag string) (int, bool) {
 	for ; i < end; i++ {
 		c, next = r[i], grab(r, i+1, end)
 		switch {
-		case quote == '\'' && c == '\\':
-			i++
-			continue
 		case quote == '\'' && c == '\'' && next == '\'':
 			i++
 			continue
